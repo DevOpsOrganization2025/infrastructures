1eed02f67a2c80e4efe5cf95b28e2feb835f8ece
Continue3010 <97082334+Continue3010@users.noreply.github.com>
2025-05-02 09:42:25 +0700

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,1 +1,16 @@
+module "eks" {
+  source         = "../../modules/eks"
+  cluster_name   = "test-cluster"
+  node_group_name = "test-node-group"
+  iam_role_arn   = ""
+  subnet_ids     = ["", "", ""]
+}
+
+output "test_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "test_cluster_ca_data" {
+  value = module.eks.cluster_ca_data
+}
 
