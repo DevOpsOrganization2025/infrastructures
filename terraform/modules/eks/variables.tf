2100972526fd93cca81942da1caae7ba0d85b1c2
Continue3010 <97082334+Continue3010@users.noreply.github.com>
2025-05-02 21:30:24 +0700

    Update variables.tf

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -8,13 +8,17 @@ variable "node_group_name" {
   type        = string
 }
 
-variable "iam_role_arn" {
-  description = "The IAM role ARN to associate with the cluster"
+variable "cluster_iam_role_arn" {
+  description = "The IAM role ARN for EKS control plane"
+  type        = string
+}
+
+variable "node_iam_role_arn" {
+  description = "The IAM role ARN for node group (EC2 workers)"
   type        = string
 }
 
 variable "subnet_ids" {
-  description = "The subnet IDs for the EKS cluster"
+  description = "List of subnet IDs to launch the EKS cluster into"
   type        = list(string)
 }
-
