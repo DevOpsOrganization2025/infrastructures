007860dbd707696676bfc33d89269913ee699c9c
Continue3010 <97082334+Continue3010@users.noreply.github.com>
2025-05-02 09:39:48 +0700

    Update variables.tf

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,1 +1,20 @@
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
+
+variable "node_group_name" {
+  description = "The name of the node group"
+  type        = string
+}
+
+variable "iam_role_arn" {
+  description = "The IAM role ARN to associate with the cluster"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "The subnet IDs for the EKS cluster"
+  type        = list(string)
+}
 
