936d036440a4f1018ccca7927aa2e0d8a3ae02fe
Continue3010 <97082334+Continue3010@users.noreply.github.com>
2025-05-02 09:42:53 +0700

    Update main.tf

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -1,1 +1,16 @@
+module "eks" {
+  source         = "../../modules/eks"
+  cluster_name   = "staging-cluster"
+  node_group_name = "staging-node-group"
+  iam_role_arn   = ""
+  subnet_ids     = ["", "", ""]
+}
+
+output "staging_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "staging_cluster_ca_data" {
+  value = module.eks.cluster_ca_data
+}
 
