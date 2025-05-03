360ee67e7cc4ddcd6bb29e8dfb7f7dc45c27b233
Continue3010 <97082334+Continue3010@users.noreply.github.com>
2025-05-03 09:32:50 +0700

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -2,7 +2,7 @@ module "eks" {
   source         = "git::https://github.com/DevOpsOrganization2025/infrastructures.git//terraform/modules/eks?ref=main"
   cluster_name   = "test-cluster"
   node_group_name = "test-node-group"
-  iam_role_arn   = "arn:aws:iam::423623850991:role/eks-cluster-role"
+  cluster_iam_role_arn  = "arn:aws:iam::423623850991:role/eks-cluster-role"
   node_iam_role_arn    ="arn:aws:iam::423623850991:instance-profile/eks-node-group-role"
   subnet_ids     = ["subnet-057a15a690f42b12b", "subnet-0a6a1a04a69320d91", "subnet-092f3cb9cd7352383","subnet-0cc19f52319fac8f9"]
 }
