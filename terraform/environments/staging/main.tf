7354d5d0da9c0866376f6e88cefd620334d4ff4d
Continue3010 <97082334+Continue3010@users.noreply.github.com>
2025-05-03 15:25:41 +0700

    Update main.tf

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -1,6 +1,6 @@
 
 module "eks" {
-  source         = "../../modules/eks"
+  source         = "git::https://github.com/DevOpsOrganization2025/infrastructures.git//terraform/modules/eks?ref=main"
   cluster_name   = "staging-cluster"
   node_group_name = "staging-node-group"
   iam_role_arn   = "arn:aws:iam::423623850991:role/eks-cluster-role"
