770224a6d138b8d2fbf6bca09775780ef5727ca9
Continue3010 <97082334+Continue3010@users.noreply.github.com>
2025-05-03 15:51:22 +0700

    Update main.tf

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -3,7 +3,7 @@ module "eks" {
   source         = "git::https://github.com/DevOpsOrganization2025/infrastructures.git//terraform/modules/eks?ref=main"
   cluster_name   = "prod-cluster"
   node_group_name = "prod-node-group"
-  iam_role_arn   = "arn:aws:iam::423623850991:role/eks-cluster-role"
+  cluster_iam_role_arn   = "arn:aws:iam::423623850991:role/eks-cluster-role"
   node_iam_role_arn    ="arn:aws:iam::423623850991:instance-profile/eks-node-group-role"
   subnet_ids     = ["subnet-092523024474ab87b", "subnet-06c071380d47fc056", "subnet-076a759129dde5264","subnet-03f084a71a11254e7"]
 }
