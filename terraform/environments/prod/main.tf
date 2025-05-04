c119788c1e6e59f501571fd1bae06026662ac619
Continue3010 <97082334+Continue3010@users.noreply.github.com>
2025-05-04 09:26:51 +0700

    Update main.tf

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -4,8 +4,8 @@ module "eks" {
   cluster_name   = "prod-cluster"
   node_group_name = "prod-node-group"
   cluster_iam_role_arn   = "arn:aws:iam::423623850991:role/eks-cluster-role"
-  node_iam_role_arn    ="arn:aws:iam::423623850991:instance-profile/eks-node-group-role"
-  subnet_ids     = ["subnet-092523024474ab87b", "subnet-06c071380d47fc056", "subnet-076a759129dde5264","subnet-03f084a71a11254e7"]
+  node_iam_role_arn    ="arn:aws:iam::423623850991:role/eks-node-group-role"
+  subnet_ids     = [ "subnet-076a759129dde5264","subnet-03f084a71a11254e7"]
 }
 
 output "test_cluster_endpoint" {
